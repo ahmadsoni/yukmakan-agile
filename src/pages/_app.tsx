0129ad147d6a2808cb4de3dfd23b6997d40f0227
ahmadsoni <ahmadshonhaji476@gmail.com>
2023-02-27 23:25:42 +0700

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import type { AppProps } from 'next/app'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+import '@/styles/globals.css'
+import { NextPageWithLayout } from '@/utils/types'
+
+const queryClient = new QueryClient()
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  if (Component.getLayout) {
+    return Component.getLayout(
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    )
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
